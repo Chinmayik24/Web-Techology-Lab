e839adc3ba4f05bc4e1e74f427d475b071e53db1
Chinmayi K <chinmayik24@gmail.com>
2024-07-25 11:26:59 +0530

    Create 10a.php

---
+++ 10/10a.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Date</title>
+  <head>
+  <body>
+    <?php 
+      echo "Today is " .date("d-m-y")."<br>";
+      echo "Today is " .date("l");
+    ?>
+  </body>
+</html>
